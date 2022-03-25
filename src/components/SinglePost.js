a488898bebedf073f5ee356e7c85cc2b618fb5c9
Milica <pavlovicm995@yahoo.com>
2022-03-25 16:50:51 +0100

    Deseti zadatak

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -2,23 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ServicePosts from "../services/ServicePosts";
 import AddComment from "./AddComment";
+import useFormattedDate from "../hooks/useFormattedDate";
 
 function SinglePost() {
   const [post, setPost] = useState("");
   const { id } = useParams();
+  const [date, setDate] = useState("0");
 
   async function fetchPost() {
     const data = await ServicePosts.get(id);
     setPost(data);
+    setDate(data.createdAt);
   }
 
   useEffect(() => {
     fetchPost();
   }, []);
 
+  const fromattedDate = useFormattedDate(date);
+
   return (
     <div>
-      <h3>{post.title}</h3>
+      <h3>{post.title}</h3><em style={{ fontSize: "10px" }}>{fromattedDate}</em>
       <p>{post.text}</p>
       <h5>Comments: </h5>
       <ul>
